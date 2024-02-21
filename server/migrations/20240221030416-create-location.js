355ca585af8aed959bfbbc12a5ea15e034f4dd93
Duan Chen <duanchen2004@gmail.com>
2024-02-20 19:12:23 -0800

    Created model for location table

---
+++ server/migrations/20240221030416-create-location.js
@@ -0,0 +1,43 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Locations', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.TEXT
+      },
+      address1: {
+        type: Sequelize.TEXT
+      },
+      address2: {
+        type: Sequelize.TEXT
+      },
+      city: {
+        type: Sequelize.TEXT
+      },
+      state: {
+        type: Sequelize.TEXT
+      },
+      zipCode: {
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Locations');
+  }
+};
