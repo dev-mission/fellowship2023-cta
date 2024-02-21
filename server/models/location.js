4c73a7054c5b4306a8629e1f3ee069cf9a59c557
duanhash <122394349+duanhash@users.noreply.github.com>
2024-02-20 19:17:27 -0800

    Created model for location table (#10)

---
+++ server/models/location.js
@@ -0,0 +1,28 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Location extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Location.init({
+    name: DataTypes.TEXT,
+    address1: DataTypes.TEXT,
+    address2: DataTypes.TEXT,
+    city: DataTypes.TEXT,
+    state: DataTypes.TEXT,
+    zipCode: DataTypes.TEXT
+  }, {
+    sequelize,
+    modelName: 'Location',
+  });
+  return Location;
+};
