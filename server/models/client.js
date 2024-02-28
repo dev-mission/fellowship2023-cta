972e5f296b268e3bb5f9c67e7bfa037a469bc197
Nichole Guan <nicholeguan2@gmail.com>
2024-02-27 16:49:22 -0800

    created client model

---
+++ server/models/client.js
@@ -0,0 +1,32 @@
+import { Model } from 'sequelize';
+
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Client extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Client.init({
+    firstName: DataTypes.TEXT,
+    lastName: DataTypes.TEXT,
+    age: DataTypes.INTEGER,
+    phone: DataTypes.TEXT,
+    email: DataTypes.TEXT,
+    ethnicity: DataTypes.TEXT,
+    address: DataTypes.TEXT,
+    gender: DataTypes.TEXT,
+    language: DataTypes.TEXT
+  }, {
+    sequelize,
+    modelName: 'Client',
+  });
+  return Client;
+};
