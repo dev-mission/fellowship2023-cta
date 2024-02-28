bcfaf1869cf15c91726ef64219cce98f12cf7683
Duan Chen <duanchen2004@gmail.com>
2024-02-27 20:31:44 -0800

    Fixed linter errors

--- server/models/donor.js
+++ server/models/donor.js
@@ -7,7 +7,7 @@ export default function (sequelize, DataTypes) {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
+    static associate(/*models*/) {
       // define association here
     }
   }
