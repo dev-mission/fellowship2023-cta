09547fbdafc25e94d52172452652a24e1748a368
Justin <justin123yu@gmail.com>
2024-03-12 14:04:19 -0700

    Errors In fixture. Duplicated key

--- server/test/routes/api/ticket.js
+++ server/test/routes/api/ticket.js
@@ -6,7 +6,7 @@ import helper from '../../helper.js';
 import models from "../../../models/index.js"
 import app from '../../../app.js';
 
-describe('/api/restaurant', () => {
+describe('/api/ticket', () => {
   let testSession;
 
   beforeEach(async () => {
@@ -17,29 +17,36 @@ describe('/api/restaurant', () => {
   it("Delete a ticket", async () => {
     const response = await testSession.delete('/api/ticket/1').expect(StatusCodes.OK);
     const records = await models.Restaurant.findByPk(response.body.id);
+    console.log(response);
     assert.deepStrictEqual(records, null);
   });
 
   it("Update a ticket information", async () => {
     const response = await testSession.patch('/api/ticket/1').send({
-      Name: "Hello World",
-      Location: "temp",
-      Rating: 3,
-      Comment: "It's a restaurant",
-      Map: "https://goo.gl/maps/123"
+        problem: "Retested Probolem",
+        troubleshooting: "Retested Troubleshooting",
     }).expect(StatusCodes.OK);
     const records = await models.Restaurant.findByPk(response.body.id);
-    assert.deepStrictEqual(records.Name, "Hello World");
-    assert.deepStrictEqual(records.Location, "temp");
+    assert.deepStrictEqual(records.problem, "Retested Probolem");
+    assert.deepStrictEqual(records.troubleshooting, "Retested Troubleshooting");
   });
 
   it("Create a new ticket", async () => { 
     const response = await testSession.post('/api/ticket').send({
-      Name: "Hello World",
-      Location: "temp",
-      Rating: 3,
-      Comment: "It's a restaurant",
-      Map: "https://goo.gl/maps/123"
+      "AppointmentId": 1,
+      "ClientId": 1,
+      "LocationId": 1,
+      "UserId": 1,
+      "DeviceId": 1,
+      "problem": "TESTING TICKETS 3",
+      "troubleshooting": "TESTING TICKETS 3",
+      "resolution": "TESTING TICKETS 3",
+      "dateOn": "2024-03-04",
+      "timeInAt": "2024-03-04",
+      "timeOutAt": "2024-03-04",
+      "totalTime": "2024-03-04",
+      "hasCharger": true,
+      "notes": "TESTING TICKETS"
     }).expect(StatusCodes.CREATED);
     const records = await models.Restaurant.findByPk(response.body.id);
     assert.deepStrictEqual(records.Name, "Hello World");
@@ -53,6 +60,6 @@ describe('/api/restaurant', () => {
 
   it("fetch a single item from the ticket table", async () => {
     const response = await testSession.get('/api/ticket/1').expect(StatusCodes.OK);
-    assert.deepStrictEqual(response.body?.Name, "Applebee's");
+    assert.deepStrictEqual(response.body?.problem, "Broken screen");
   });
 });
