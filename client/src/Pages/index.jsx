4106149656eec5c63cc57c33ce61b964eab66d1b
duanhash <122394349+duanhash@users.noreply.github.com>
2024-03-19 19:36:25 -0700

    Added pages folder for all routes (#65)
    * Added pages folder for all routes
    
    * Ran pp & lint
    
    ---------
    
    Co-authored-by: duanhash <duanchenwork@gmail.com>

---
+++ client/src/Pages/index.jsx
@@ -0,0 +1,9 @@
+import Appointments from './Appointments';
+import Tickets from './Tickets';
+import Clients from './Clients';
+import Inventory from './Inventory';
+import Locations from './Locations';
+import Donors from './Donors';
+import Users from './Users';
+
+export { Appointments, Tickets, Clients, Inventory, Locations, Donors, Users };
