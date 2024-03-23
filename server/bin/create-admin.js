d8dc5976c1f15af97e8014785e0e4733762f5aaf
duanhash <122394349+duanhash@users.noreply.github.com>
2024-03-22 19:02:40 -0700

    79 modify create adminjs script (#80)
    * Finished modifying create-admin.js
    
    * Ranprettier, lint, and tests
    
    ---------
    
    Co-authored-by: duanhash <duanchenwork@gmail.com>

--- server/bin/create-admin.js
+++ server/bin/create-admin.js
@@ -10,11 +10,26 @@ if (process.argv.length != 6) {
 import bcrypt from 'bcrypt';
 import models from '../models/index.js';
 
-bcrypt.hash(process.argv[5], 10).then((hashedPassword) => {
+bcrypt.hash(process.argv[5], 10).then(async (hashedPassword) => {
+  const response = await models.Location.findByPk(1);
+
+  if (response === null) {
+    models.Location.create({
+      id: 1,
+      name: 'Dev/Mission',
+      address1: '356 Bryant St',
+      address2: 'N/A',
+      city: 'San Francisco',
+      state: 'California',
+      zipCode: '94107',
+    });
+  }
+
   models.User.create({
     firstName: process.argv[2],
     lastName: process.argv[3],
     email: process.argv[4],
+    LocationId: 1,
     hashedPassword: hashedPassword,
     isAdmin: true,
   }).then(() => {
