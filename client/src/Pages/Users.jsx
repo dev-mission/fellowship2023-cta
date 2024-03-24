029e9e86e0f300ba2b0c5e4eb89844aabe587251
duanhash <duanchenwork@gmail.com>
2024-03-23 21:24:55 -0700

    Added search bar, New button, & Users Icon to Users Page

--- client/src/Pages/Users.jsx
+++ client/src/Pages/Users.jsx
@@ -1,7 +1,19 @@
 //import React from 'react';
 
+
 const Users = () => {
-  return <div>Users</div>;
+  return (
+    <main className='container'>
+      <div className='d-flex justify-content-between align-items-center'>
+        <button type='button' className='btn btn-primary d-flex align-items-center'>New <i class="bi bi-plus-lg plus"></i></button>
+        <i className='bi bi-person-fill personIcon'>Users</i>
+        <form class='form-inline my-2 my-lg-0'>
+          <input class='form-control' type='search' placeholder='Search Users' aria-label='Search'/>
+        </form>
+      </div>
+    </main>
+
+  );
 };
 
 export default Users;
