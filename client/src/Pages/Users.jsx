c39f6bf14a735a5e58e2e751b40341f8c498f1ae
duanhash <duanchenwork@gmail.com>
2024-03-25 20:40:34 -0700

    Finished table delete functionality

--- client/src/Pages/Users.jsx
+++ client/src/Pages/Users.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import React from 'react';
 import {
   getCoreRowModel,
   getFilteredRowModel,
@@ -41,9 +42,7 @@ const columns = [
   },
 ];
 
-const Filters = ({ columnFilters, setColumnFilters }) => {
-  const userName = columnFilters.find((f) => f.id === 'firstName')?.value || '';
-
+const Filters = ({ setColumnFilters }) => {
   const onFilterChange = (id, value) => setColumnFilters((prev) => prev.filter((f) => f.id !== id).concat({ id, value }));
 
   return (
@@ -56,7 +55,6 @@ const Filters = ({ columnFilters, setColumnFilters }) => {
           type="search"
           className="form-control me-2"
           placeholder="Search Users"
-          value={userName}
           onChange={(e) => onFilterChange('firstName', e.target.value)}
         />
       </div>
@@ -65,45 +63,83 @@ const Filters = ({ columnFilters, setColumnFilters }) => {
 };
 
 Filters.propTypes = {
-  columnFilters: PropTypes.arrayOf(PropTypes.shape({ id: PropTypes.string, value: PropTypes.string })).isRequired,
   setColumnFilters: PropTypes.func.isRequired,
 };
 
-const UserTable = ({ table }) => {
+const DeleteModal = ({ toggleDeleteModal, setToggleDeleteModal, row, data, setData}) => {
+  const onDelete = async () => {
+    setToggleDeleteModal(false);
+    try {
+      await fetch(`/api/users/${row.original.id}`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      setData(data.filter((d) => d.id !== row.original.id));
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   return (
-    <table className="table mt-5">
-      <thead className="table-primary">
-        <tr>
-          {table.getHeaderGroups()[0].headers.map((header) => (
-            <th scope="col" key={header.id}>
-              {header.column.columnDef.header}
-              {header.column.getCanSort() && <i className="ms-2 bi bi-arrow-down-up" onClick={header.column.getToggleSortingHandler()} />}
-            </th>
-          ))}
-          <th scope="col">
-            <i className="bi bi-pencil-square" />
-          </th>
-          <th scope="col">
-            <i className="bi bi-trash-fill" />
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        {table.getRowModel().rows.map((row) => (
-          <tr key={row.id}>
-            {row.getVisibleCells().map((cell) => (
-              <td key={cell.id}>{flexRender(cell.column.columnDef.cell, cell.getContext())}</td>
+    <Modal show={toggleDeleteModal} onHide={() => setToggleDeleteModal(false)}>
+      <Modal.Header closeButton>
+        <Modal.Title>Delete User</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>Are you sure you want to delete this user?</Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={() => setToggleDeleteModal(false)}>
+          No
+        </Button>
+        <Button variant="danger" onClick={onDelete}>
+          Yes
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
+
+const UserTable = ({ table, data, setData }) => {
+  const [toggleDeleteModal, setToggleDeleteModal] = useState(false);
+
+  return (
+    <>
+      <table className="table mt-5">
+        <thead className="table-primary">
+          <tr>
+            {table.getHeaderGroups()[0].headers.map((header) => (
+              <th scope="col" key={header.id}>
+                {header.column.columnDef.header}
+                {header.column.getCanSort() && <i className="ms-2 bi bi-arrow-down-up" onClick={header.column.getToggleSortingHandler()} />}
+              </th>
             ))}
-            <td>
-              <i className="bi bi-pencil" />
-            </td>
-            <td>
-              <i className="bi bi-x-lg" />
-            </td>
+            <th scope="col">
+              <i className="bi bi-pencil-square" />
+            </th>
+            <th scope="col">
+              <i className="bi bi-trash-fill" />
+            </th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {table.getRowModel().rows.map((row) => (
+            <tr key={row.id}>
+              {row.getVisibleCells().map((cell) => (
+                <td key={cell.id}>{flexRender(cell.column.columnDef.cell, cell.getContext())}</td>
+              ))}  
+              <td>
+                <i className="bi bi-pencil" />
+              </td>
+              <td>
+                <i className="bi bi-x-lg" onClick={() => setToggleDeleteModal(true)}/>
+              </td>
+              <DeleteModal toggleDeleteModal={toggleDeleteModal} setToggleDeleteModal={setToggleDeleteModal} row={row} data={data} setData={setData}/>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
   );
 };
 
@@ -114,18 +150,35 @@ UserTable.propTypes = {
   }).isRequired,
 };
 
-const UserModal = ({ toggleModal, setToggleModal }) => {
+const UserModal = ({ toggleUserModal, setToggleUserModal }) => {
   const [check, setCheck] = useState(false);
-  // const [data, setData] = useState({
-  //   firstName: '',
-  //   lastName: '',
-  //   email: '',
-  //   role: '',
-  //   location: '',
-  // });
+  const [data, setData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    role: '',
+    location: '',
+    isAdmin: check,
+  });
+
+  const onChange = (e) => {
+    const newData = {...data};
+    newData[e.target.name] = e.target.value;
+    setData(newData);
+  }
+
+  // const onSubmit = async (e) => {
+  //   e.preventDefault();
+  //   let method = 'POST';
+
+  //   try {
+  //     let path = '/api/users';
+
+  //   }
+  // }
 
   return (
-    <Modal show={toggleModal} onHide={() => setToggleModal(false)}>
+    <Modal show={toggleUserModal} onHide={() => setToggleUserModal(false)}>
       <Modal.Header closeButton>
         <Modal.Title>New User</Modal.Title>
       </Modal.Header>
@@ -134,15 +187,15 @@ const UserModal = ({ toggleModal, setToggleModal }) => {
           <Form>
             <Row>
               <Col xs={9} md={6}>
-                <Form.Group controlId="firstName">
+                <Form.Group controlId="firstName" >
                   <Form.Label>First Name</Form.Label>
-                  <Form.Control type="name" autoFocus />
+                  <Form.Control type="name" autoFocus onChange={onChange}/>
                 </Form.Group>
               </Col>
               <Col xs={9} md={6}>
                 <Form.Group controlId="lastName">
                   <Form.Label>Last Name</Form.Label>
-                  <Form.Control type="name" autoFocus />
+                  <Form.Control type="name" autoFocus onChange={onChange}/>
                 </Form.Group>
               </Col>
             </Row>
@@ -150,13 +203,13 @@ const UserModal = ({ toggleModal, setToggleModal }) => {
               <Col xs={12} md={8}>
                 <Form.Group controlId="email">
                   <Form.Label>Email Address</Form.Label>
-                  <Form.Control type="email" autoFocus />
+                  <Form.Control type="email" autoFocus onChange={onChange}/>
                 </Form.Group>
               </Col>
               <Col xs={6} md={4}>
                 <Form.Group controlId="role">
                   <Form.Label>Role</Form.Label>
-                  <Form.Control type="name" autoFocus />
+                  <Form.Control type="name" autoFocus onChange={onChange}/>
                 </Form.Group>
               </Col>
             </Row>
@@ -164,7 +217,7 @@ const UserModal = ({ toggleModal, setToggleModal }) => {
               <Col xs={12} md={8}>
                 <Form.Group controlId="location">
                   <Form.Label>Location</Form.Label>
-                  <Form.Control type="name" autoFocus />
+                  <Form.Control type="name" autoFocus onChange={onChange}/>
                 </Form.Group>
               </Col>
               <Col xs={6} md={4}>
@@ -175,10 +228,10 @@ const UserModal = ({ toggleModal, setToggleModal }) => {
         </Container>
       </Modal.Body>
       <Modal.Footer>
-        <Button variant="secondary" onClick={() => setToggleModal(false)}>
+        <Button variant="secondary" onClick={() => setToggleUserModal(false)}>
           Close
         </Button>
-        <Button variant="primary" onClick={() => setToggleModal(false)}>
+        <Button variant="primary" onClick={() => setToggleUserModal(false)}>
           Submit
         </Button>
       </Modal.Footer>
@@ -187,14 +240,14 @@ const UserModal = ({ toggleModal, setToggleModal }) => {
 };
 
 UserModal.propTypes = {
-  toggleModal: PropTypes.bool.isRequired,
-  setToggleModal: PropTypes.func.isRequired,
+  toggleUserModal: PropTypes.bool.isRequired,
+  setToggleUserModal: PropTypes.func.isRequired,
 };
 
 const Users = () => {
   const [data, setData] = useState();
   const [columnFilters, setColumnFilters] = useState([]);
-  const [toggleModal, setToggleModal] = useState(false);
+  const [toggleUserModal, setToggleUserModal] = useState(false);
 
   useEffect(() => {
     fetch('/api/users')
@@ -207,6 +260,7 @@ const Users = () => {
     columns,
     state: {
       columnFilters,
+      data,
     },
     getCoreRowModel: getCoreRowModel(),
     getFilteredRowModel: getFilteredRowModel(),
@@ -214,17 +268,19 @@ const Users = () => {
     getPaginationRowModel: getPaginationRowModel(),
   });
 
+  console.log(data);
+
   return (
     <main className="container">
       <div className="d-flex justify-content-between align-items-center mt-5">
-        <button type="button" className="btn btn-primary d-flex align-items-center" onClick={() => setToggleModal(true)}>
+        <button type="button" className="btn btn-primary d-flex align-items-center" onClick={() => setToggleUserModal(true)}>
           New <i className="bi bi-plus-lg" />
         </button>
-        <UserModal toggleModal={toggleModal} setToggleModal={setToggleModal} />
+        <UserModal toggleUserModal={toggleUserModal} setToggleUserModal={setToggleUserModal} />
         <i className="bi bi-person-fill">Users</i>
-        <Filters columnFilters={columnFilters} setColumnFilters={setColumnFilters} />
+        <Filters setColumnFilters={setColumnFilters} />
       </div>
-      <UserTable table={table} />
+      <UserTable table={table} data={data} setData={setData}/>
       <p>
         Page {table.getState().pagination.pageIndex + 1} of {table.getPageCount()}
       </p>
