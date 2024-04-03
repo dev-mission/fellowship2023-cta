2886b994394b261293cd6fc2bee729a810eb191e
carerit <kevnli2002@gmail.com>
2024-04-02 19:43:53 -0700

    completed refactoring the table for device

--- client/src/Pages/Inventory.jsx
+++ client/src/Pages/Inventory.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import {
   getCoreRowModel,
   getFilteredRowModel,
-  getSortedRowModel,
+  // getSortedRowModel,
   getPaginationRowModel,
   useReactTable,
   flexRender,
@@ -11,33 +11,66 @@ import PropTypes from 'prop-types';
 import { Modal, Button, Form, Container, Row, Col } from 'react-bootstrap';
 
 const columns = [
+  
   {
-    accessorKey: 'firstName',
-    header: 'First',
+    accessorKey: 'id',
+    header: 'Device#',
     enableColumnFilter: true,
   },
   {
-    accessorKey: 'lastName',
-    header: 'Last',
+    accessorKey: 'deviceType',
+    header: 'Device Type',
     enableColumnFilter: true,
   },
   {
-    accessorKey: 'LocationId',
-    header: 'Location',
-    enableColumnFilter: true,
-    enableSorting: false,
+    accessorKey: 'model',
+    header: 'model',
+    enableColumnFilter: false,
   },
   {
-    accessorKey: 'role',
-    header: 'Role',
+    accessorKey: 'brand',
+    header: 'Brand',
     enableColumnFilter: true,
-    enableSorting: false,
   },
   {
-    accessorKey: 'email',
-    header: 'Email',
+    accessorKey: 'serialNum',
+    header: 'S/N',
     enableColumnFilter: true,
-    enableSorting: false,
+  },
+  {
+    accessorKey: 'cpu',
+    header: 'CPU',
+    enableColumnFilter: true,
+  },
+  {
+    accessorKey: 'ram',
+    header: 'RAM',
+    enableColumnFilter: true,
+  },
+  {
+    accessorKey: 'os',
+    header: 'OS',
+    enableColumnFilter: true,
+  },
+  {
+    accessorKey: 'username',
+    header: 'Username',
+    enableColumnFilter: true,
+  },
+  {
+    accessorKey: 'password',
+    header: 'Password',
+    enableColumnFilter: true,
+  },
+  {
+    accessorKey: 'condition',
+    header: 'Condition',
+    enableColumnFilter: true,
+  },
+  {
+    accessorKey: 'value',
+    header: 'Value',
+    enableColumnFilter: true,
   },
 ];
 
@@ -53,8 +86,8 @@ const Filters = ({ setColumnFilters }) => {
         <input
           type="search"
           className="form-control me-2"
-          placeholder="Search Users"
-          onChange={(e) => onFilterChange('firstName', e.target.value)}
+          placeholder="Search Devices"
+          onChange={(e) => onFilterChange('name', e.target.value)}
         />
       </div>
     </form>
@@ -69,7 +102,7 @@ const DeleteModal = ({ toggleDeleteModal, setToggleDeleteModal, row, data, setDa
   const onDelete = async () => {
     setToggleDeleteModal(false);
     try {
-      await fetch(`/api/users/${row.original.id}`, {
+      await fetch(`/api/devices/${row.original.id}`, {
         method: 'DELETE',
         headers: {
           'Content-Type': 'application/json',
@@ -84,9 +117,9 @@ const DeleteModal = ({ toggleDeleteModal, setToggleDeleteModal, row, data, setDa
   return (
     <Modal show={toggleDeleteModal} onHide={() => setToggleDeleteModal(false)}>
       <Modal.Header closeButton>
-        <Modal.Title>Delete User</Modal.Title>
+        <Modal.Title>Delete Device</Modal.Title>
       </Modal.Header>
-      <Modal.Body>Are you sure you want to delete this user?</Modal.Body>
+      <Modal.Body>Are you sure you want to delete this device?</Modal.Body>
       <Modal.Footer>
         <Button variant="secondary" onClick={() => setToggleDeleteModal(false)}>
           No
@@ -107,12 +140,33 @@ DeleteModal.propTypes = {
       id: PropTypes.number,
     }),
   }).isRequired,
-  data: PropTypes.arrayOf(PropTypes.object).isRequired,
+  data: PropTypes.arrayOf(PropTypes.object),
   setData: PropTypes.func.isRequired,
 };
 
-const UserTable = ({ table, data, setData }) => {
+const DeviceTable = ({ table, data, setData, setToggleDeviceModal }) => {
   const [toggleDeleteModal, setToggleDeleteModal] = useState(false);
+  const [propRow, setPropRow] = useState({});
+
+  const onDelete = (row) => (e) => {
+    try {
+      setToggleDeleteModal(true);
+      setPropRow(row);
+    } catch (err) {
+      console.log(err);
+      console.log(e);
+    }
+  };
+
+  const onEdit = (row) => (e) => {
+    try {
+      setToggleDeviceModal(true);
+      setPropRow(row);
+    } catch (err) {
+      console.log(err);
+      console.log(e);
+    }
+  };
 
   return (
     <>
@@ -122,7 +176,6 @@ const UserTable = ({ table, data, setData }) => {
             {table.getHeaderGroups()[0].headers.map((header) => (
               <th scope="col" key={header.id}>
                 {header.column.columnDef.header}
-                {header.column.getCanSort() && <i className="ms-2 bi bi-arrow-down-up" onClick={header.column.getToggleSortingHandler()} />}
               </th>
             ))}
             <th scope="col">
@@ -140,117 +193,171 @@ const UserTable = ({ table, data, setData }) => {
                 <td key={cell.id}>{flexRender(cell.column.columnDef.cell, cell.getContext())}</td>
               ))}
               <td>
-                <i className="bi bi-pencil" />
+                <i className="bi bi-pencil" onClick={onEdit(row)}/>
               </td>
               <td>
-                <i className="bi bi-x-lg" onClick={() => setToggleDeleteModal(true)} />
+                <i className="bi bi-x-lg" onClick={onDelete(row)}  />
               </td>
-              <DeleteModal
-                toggleDeleteModal={toggleDeleteModal}
-                setToggleDeleteModal={setToggleDeleteModal}
-                row={row}
-                data={data}
-                setData={setData}
-              />
             </tr>
           ))}
         </tbody>
+        <DeleteModal
+          toggleDeleteModal={toggleDeleteModal}
+          setToggleDeleteModal={setToggleDeleteModal}
+          row={propRow}
+          isRequired
+          data={data}
+          setData={setData}
+        />
+        <DeviceModal row={propRow} />
       </table>
     </>
   );
 };
 
-UserTable.propTypes = {
+DeviceTable.propTypes = {
   table: PropTypes.shape({
     getHeaderGroups: PropTypes.func.isRequired,
     getRowModel: PropTypes.func.isRequired,
   }).isRequired,
   data: PropTypes.arrayOf(PropTypes.object),
   setData: PropTypes.func.isRequired,
+  setToggleDeviceModal: PropTypes.func.isRequired,
 };
 
-const UserModal = ({ toggleUserModal, setToggleUserModal }) => {
-  const [check, setCheck] = useState(false);
-  const [data, setData] = useState({
-    firstName: '',
-    lastName: '',
-    email: '',
-    role: '',
-    location: '',
-    isAdmin: check,
+const DeviceModal = ({ toggleDeviceModal, setToggleDeviceModal, data, setData }) => {
+
+/*
+"deviceType": "Laptop",
+      "model": "MacBook Pro",
+      "brand": "Apple",
+      "serialNum": "1234567890",
+      "cpu": "Intel Core i7",
+      "ram": "16GB",
+      "os": "MacOS",
+      "username": "admin",
+      "password": "admin",
+      "condition": "Good",
+      "value": "1000.00",
+      "notes": "This is a test device."
+
+*/
+
+  const [addData, setAddData] = useState({
+    deviceType: '',
+    model: '',
+    brand: '',
+    serialNum: '',
+    cpu: '',
+    ram: '',
+    os: '',
+    username: '',
+    password: '',
+    condition: '',
+    value: '',
+    notes: '',
   });
 
   const onChange = (e) => {
-    const newData = { ...data };
+    const newData = { ...addData };
     newData[e.target.name] = e.target.value;
-    setData(newData);
+    setAddData(newData);
+    console.log(addData);
   };
 
-  // const onSubmit = async (e) => {
-  //   e.preventDefault();
-  //   let method = 'POST';
+  const onSubmit = async (e) => {
+    setToggleDeviceModal(false);
+    e.preventDefault();
+    try {
+      await fetch('/api/devices', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(addData),
+      });
 
-  //   try {
-  //     let path = '/api/users';
-
-  //   }
-  // }
+      setData([...data, addData]);
+      console.log(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
-    <Modal show={toggleUserModal} onHide={() => setToggleUserModal(false)}>
+    <Modal show={toggleDeviceModal} onHide={() => setToggleDeviceModal(false)}>
       <Modal.Header closeButton>
-        <Modal.Title>New User</Modal.Title>
+        <Modal.Title>New Device</Modal.Title>
       </Modal.Header>
       <Modal.Body>
         <Container>
           <Form>
             <Row>
-              <Col xs={9} md={6}>
-                <Form.Group controlId="firstName">
-                  <Form.Label>First Name</Form.Label>
-                  <Form.Control type="name" autoFocus onChange={onChange} />
+              <Col xs={18} md={8}>
+                <Form.Group controlId="name">
+                  <Form.Label>Device Name</Form.Label>
+                  <Form.Control name="name" autoFocus value={addData.name} onChange={onChange} />
                 </Form.Group>
               </Col>
-              <Col xs={9} md={6}>
-                <Form.Group controlId="lastName">
-                  <Form.Label>Last Name</Form.Label>
-                  <Form.Control type="name" autoFocus onChange={onChange} />
+              <Col xs={18} md={4}>
+                <Form.Group controlId="phone">
+                  <Form.Label>Device Phone</Form.Label>
+                  <Form.Control name="phone" autoFocus value={addData.phone} onChange={onChange} />
+                </Form.Group>
+              </Col>
+            </Row>
+
+            <Row>
+              <Col xs={18} md={12}>
+                <Form.Group controlId="email">
+                  <Form.Label>Device Email</Form.Label>
+                  <Form.Control name="email" autoFocus value={addData.email} onChange={onChange} />
                 </Form.Group>
               </Col>
             </Row>
             <Row>
-              <Col xs={12} md={8}>
-                <Form.Group controlId="email">
-                  <Form.Label>Email Address</Form.Label>
-                  <Form.Control type="email" autoFocus onChange={onChange} />
+              <Col xs={18} md={8}>
+                <Form.Group controlId="address1">
+                  <Form.Label>Address 1</Form.Label>
+                  <Form.Control name="address1" autoFocus value={addData.address1} onChange={onChange} />
                 </Form.Group>
               </Col>
-              <Col xs={6} md={4}>
-                <Form.Group controlId="role">
-                  <Form.Label>Role</Form.Label>
-                  <Form.Control type="name" autoFocus onChange={onChange} />
+              <Col xs={18} md={4}>
+                <Form.Group controlId="address2">
+                  <Form.Label>Address 2</Form.Label>
+                  <Form.Control name="address2" autoFocus value={addData.address2} placeholder="Ex: Apt 1" onChange={onChange} />
                 </Form.Group>
               </Col>
             </Row>
-            <Row className="d-flex align-items-end">
-              <Col xs={12} md={8}>
-                <Form.Group controlId="location">
-                  <Form.Label>Location</Form.Label>
-                  <Form.Control type="name" autoFocus onChange={onChange} />
+
+            <Row>
+              <Col xs={9} md={6}>
+                <Form.Group controlId="city">
+                  <Form.Label>City</Form.Label>
+                  <Form.Control name="city" autoFocus value={addData.city} onChange={onChange} />
                 </Form.Group>
               </Col>
-              <Col xs={6} md={4}>
-                <Form.Check type="switch" id="custom-switch" label="Admin" checked={check} onChange={() => setCheck(!check)} />
+              <Col xs={4.5} md={3}>
+                <Form.Group controlId="state">
+                  <Form.Label>State</Form.Label>
+                  <Form.Control name="state" autoFocus value={addData.state} onChange={onChange} />
+                </Form.Group>
+              </Col>
+              <Col xs={4.5} md={3}>
+                <Form.Group controlId="zip">
+                  <Form.Label>Zip Code</Form.Label>
+                  <Form.Control name="zip" autoFocus value={addData.zip} onChange={onChange} />
+                </Form.Group>
               </Col>
             </Row>
           </Form>
         </Container>
       </Modal.Body>
       <Modal.Footer>
-        <Button variant="secondary" onClick={() => setToggleUserModal(false)}>
+        <Button variant="secondary" onClick={() => setToggleDeviceModal(false)}>
           Close
         </Button>
-        <Button variant="primary" onClick={() => setToggleUserModal(false)}>
+        <Button variant="primary" onClick={onSubmit} type="submit">
           Submit
         </Button>
       </Modal.Footer>
@@ -258,18 +365,21 @@ const UserModal = ({ toggleUserModal, setToggleUserModal }) => {
   );
 };
 
-UserModal.propTypes = {
-  toggleUserModal: PropTypes.bool.isRequired,
-  setToggleUserModal: PropTypes.func.isRequired,
+DeviceModal.propTypes = {
+  toggleDeviceModal: PropTypes.bool.isRequired,
+  setToggleDeviceModal: PropTypes.func.isRequired,
+  data: PropTypes.arrayOf(PropTypes.object).isRequired,
+  setData: PropTypes.func.isRequired,
+  row: PropTypes.object,
 };
 
-const Users = () => {
+const Devices = () => {
   const [data, setData] = useState();
   const [columnFilters, setColumnFilters] = useState([]);
-  const [toggleUserModal, setToggleUserModal] = useState(false);
+  const [toggleDeviceModal, setToggleDeviceModal] = useState(false);
 
   useEffect(() => {
-    fetch('/api/users')
+    fetch('/api/devices')
       .then((res) => res.json())
       .then((data) => setData(data));
   }, []);
@@ -283,21 +393,26 @@ const Users = () => {
     },
     getCoreRowModel: getCoreRowModel(),
     getFilteredRowModel: getFilteredRowModel(),
-    getSortedRowModel: getSortedRowModel(),
+    // getSortedRowModel: getSortedRowModel(),
     getPaginationRowModel: getPaginationRowModel(),
   });
 
   return (
     <main className="container">
       <div className="d-flex justify-content-between align-items-center mt-5">
-        <button type="button" className="btn btn-primary d-flex align-items-center" onClick={() => setToggleUserModal(true)}>
+        <button type="button" className="btn btn-primary d-flex align-items-center" onClick={() => setToggleDeviceModal(true)}>
           New <i className="bi bi-plus-lg" />
         </button>
-        <UserModal toggleUserModal={toggleUserModal} setToggleUserModal={setToggleUserModal} />
-        <i className="bi bi-person-fill">Users</i>
+        <DeviceModal
+          toggleDeviceModal={toggleDeviceModal}
+          setToggleDeviceModal={setToggleDeviceModal}
+          data={data}
+          setData={setData}
+        />
+        <i className="bi title-icon bi-card-checklist">Devices</i>
         <Filters setColumnFilters={setColumnFilters} />
       </div>
-      <UserTable table={table} data={data} setData={setData} />
+      <DeviceTable table={table} data={data} setData={setData} setToggleDeviceModal={setToggleDeviceModal}/>
       <p>
         Page {table.getState().pagination.pageIndex + 1} of {table.getPageCount()}
       </p>
@@ -313,4 +428,4 @@ const Users = () => {
   );
 };
 
-export default Users;
+export default Devices;
