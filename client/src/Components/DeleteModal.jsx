e45b276a609a9c24d3b3d3b1b828e4ef06fbf0e5
duanhash <122394349+duanhash@users.noreply.github.com>
2024-04-05 21:35:10 -0700

    Added modal to display when trying to delete row that is referenced in a table in another page (#110)
    Co-authored-by: duanhash <duanchenwork@gmail.com>

--- client/src/Components/DeleteModal.jsx
+++ client/src/Components/DeleteModal.jsx
@@ -1,39 +1,57 @@
 import { Modal, Button } from 'react-bootstrap';
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 
 const DeleteModal = ({ toggleDeleteModal, setToggleDeleteModal, row, data, setData, model }) => {
+  const [toggleErrorModal, setToggleErrorModal] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+
   const onDelete = async () => {
     setToggleDeleteModal(false);
-    try {
-      await fetch(`/api/${model}/${row.original.id}`, {
-        method: 'DELETE',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-      setData(data.filter((d) => d.id !== row.original.id));
-    } catch (err) {
-      console.log(err);
-    }
+    await fetch(`/api/${model}/${row.original.id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }).then(async (response) => {
+      const json = await response.json();
+      if (json.error === 'error') {
+        setToggleErrorModal(true);
+        setErrorMessage(json.message.split(' ').pop().slice(1, -2));
+      } else {
+        setData(data.filter((d) => d.id !== row.original.id));
+      }
+    });
   };
 
-  console.log(row);
-
   return (
-    <Modal show={toggleDeleteModal} onHide={() => setToggleDeleteModal(false)}>
-      <Modal.Header closeButton>
-        <Modal.Title>Delete</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>Are you sure you want to delete this location?</Modal.Body>
-      <Modal.Footer>
-        <Button variant="secondary" onClick={() => setToggleDeleteModal(false)}>
-          No
-        </Button>
-        <Button variant="danger" onClick={onDelete}>
-          Yes
-        </Button>
-      </Modal.Footer>
-    </Modal>
+    <>
+      <Modal show={toggleDeleteModal} onHide={() => setToggleDeleteModal(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>Delete</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Are you sure you want to delete this location?</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => setToggleDeleteModal(false)}>
+            No
+          </Button>
+          <Button variant="danger" onClick={onDelete}>
+            Yes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      <Modal show={toggleErrorModal} onHide={() => setToggleErrorModal(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>Error</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>{'Must remove from table in ' + errorMessage + ' page first.'}</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => setToggleErrorModal(false)}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 };
 
