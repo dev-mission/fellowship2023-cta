c9467876b5dafa00681907625ac251d55613f690
carerit <kevnli2002@gmail.com>
2024-04-09 00:16:02 -0700

    modulized Donor Page

---
+++ client/src/Components/index.jsx
@@ -0,0 +1,7 @@
+import AddLocationModal from './AddLocationModal';
+import EditLocationModal from './EditLocationModal';
+import AddDonorModal from './AddDonorModal';
+import EditDonorModal from './EditDonorModal';
+import DeleteModal from './DeleteModal';
+
+export { AddLocationModal, EditLocationModal, DeleteModal, AddDonorModal, EditDonorModal };
--- client/src/Pages/Donors.jsx
+++ client/src/Pages/Donors.jsx
@@ -3,12 +3,14 @@ import {
   getCoreRowModel,
   getFilteredRowModel,
   // getSortedRowModel,
-  getPaginationRowModel,
   useReactTable,
   flexRender,
 } from '@tanstack/react-table';
 import PropTypes from 'prop-types';
-import { Modal, Button, Form, Container, Row, Col } from 'react-bootstrap';
+import { AddDonorModal, EditDonorModal, DeleteModal } from '../Components';
+import Api from '../Api';
+import Pagination from '../Components/Pagination';
+import { useDonor } from 'react-router-dom';
 
 const columns = [
   {
@@ -74,57 +76,13 @@ const Filters = ({ setColumnFilters }) => {
 };
 
 Filters.propTypes = {
-  setColumnFilters: PropTypes.func.isRequired,
+  setColumnFilters: PropTypes.func,
 };
 
-const DeleteModal = ({ toggleDeleteModal, setToggleDeleteModal, row, data, setData }) => {
-  const onDelete = async () => {
-    setToggleDeleteModal(false);
-    try {
-      await fetch(`/api/donors/${row.original.id}`, {
-        method: 'DELETE',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-      setData(data.filter((d) => d.id !== row.original.id));
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
-  return (
-    <Modal show={toggleDeleteModal} onHide={() => setToggleDeleteModal(false)}>
-      <Modal.Header closeButton>
-        <Modal.Title>Delete Donor</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>Are you sure you want to delete this donor?</Modal.Body>
-      <Modal.Footer>
-        <Button variant="secondary" onClick={() => setToggleDeleteModal(false)}>
-          No
-        </Button>
-        <Button variant="danger" onClick={onDelete}>
-          Yes
-        </Button>
-      </Modal.Footer>
-    </Modal>
-  );
-};
-
-DeleteModal.propTypes = {
-  toggleDeleteModal: PropTypes.bool.isRequired,
-  setToggleDeleteModal: PropTypes.func.isRequired,
-  row: PropTypes.shape({
-    original: PropTypes.shape({
-      id: PropTypes.number,
-    }),
-  }).isRequired,
-  data: PropTypes.arrayOf(PropTypes.object),
-  setData: PropTypes.func.isRequired,
-};
-
-const DonorTable = ({ table, data, setData, setToggleDonorModal }) => {
+const DonorTable = ({ table, data, setData }) => {
   const [toggleDeleteModal, setToggleDeleteModal] = useState(false);
+  const [toggleEditModal, setToggleEditModal] = useState(false);
+  const [editData, setEditData] = useState({});
   const [propRow, setPropRow] = useState({});
 
   const onDelete = (row) => (e) => {
@@ -139,8 +97,19 @@ const DonorTable = ({ table, data, setData, setToggleDonorModal }) => {
 
   const onEdit = (row) => (e) => {
     try {
-      setToggleDonorModal(true);
+      setToggleEditModal(true);
       setPropRow(row);
+      setEditData({
+        id: row.original.id,
+        name: row.original.name,
+        phone: row.original.phone,
+        email: row.original.email,
+        address1: row.original.address1,
+        address2: row.original.address2,
+        city: row.original.city,
+        state: row.original.state,
+        zipCode: row.original.zip,
+      });
     } catch (err) {
       console.log(err);
       console.log(e);
@@ -172,10 +141,10 @@ const DonorTable = ({ table, data, setData, setToggleDonorModal }) => {
                 <td key={cell.id}>{flexRender(cell.column.columnDef.cell, cell.getContext())}</td>
               ))}
               <td>
-                <i className="bi bi-pencil" onClick={onEdit(row)}/>
+                <i className="bi bi-pencil" onClick={onEdit(row)} />
               </td>
               <td>
-                <i className="bi bi-x-lg" onClick={onDelete(row)}  />
+                <i className="bi bi-x-lg" onClick={onDelete(row)} />
               </td>
             </tr>
           ))}
@@ -184,163 +153,53 @@ const DonorTable = ({ table, data, setData, setToggleDonorModal }) => {
           toggleDeleteModal={toggleDeleteModal}
           setToggleDeleteModal={setToggleDeleteModal}
           row={propRow}
-          isRequired
           data={data}
           setData={setData}
+          model="donors"
         />
-        <DonorModal row={propRow} />
+        <EditDonorModal
+          toggleEditModal={toggleEditModal}
+          setToggleEditModal={setToggleEditModal}
+          row={propRow}
+          data={data}
+          setData={setData}
+          editData={editData}
+          setEditData={setEditData}
+        />
       </table>
     </>
   );
 };
 
 DonorTable.propTypes = {
-  table: PropTypes.shape({
-    getHeaderGroups: PropTypes.func.isRequired,
-    getRowModel: PropTypes.func.isRequired,
-  }).isRequired,
-  data: PropTypes.arrayOf(PropTypes.object),
-  setData: PropTypes.func.isRequired,
-  setToggleDonorModal: PropTypes.func.isRequired,
-};
-
-const DonorModal = ({ toggleDonorModal, setToggleDonorModal, data, setData }) => {
-  const [addData, setAddData] = useState({
-    name: '',
-    phone: '',
-    email: '',
-    address1: '',
-    address2: '',
-    city: '',
-    state: '',
-    zip: '',
-  });
-
-  const onChange = (e) => {
-    const newData = { ...addData };
-    newData[e.target.name] = e.target.value;
-    setAddData(newData);
-    console.log(addData);
-  };
-
-  const onSubmit = async (e) => {
-    setToggleDonorModal(false);
-    e.preventDefault();
-    try {
-      await fetch('/api/donors', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(addData),
-      });
-
-      setData([...data, addData]);
-      console.log(data);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  return (
-    <Modal show={toggleDonorModal} onHide={() => setToggleDonorModal(false)}>
-      <Modal.Header closeButton>
-        <Modal.Title>New Donor</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <Container>
-          <Form>
-            <Row>
-              <Col xs={18} md={8}>
-                <Form.Group controlId="name">
-                  <Form.Label>Donor Name</Form.Label>
-                  <Form.Control name="name" autoFocus value={addData.name} onChange={onChange} />
-                </Form.Group>
-              </Col>
-              <Col xs={18} md={4}>
-                <Form.Group controlId="phone">
-                  <Form.Label>Donor Phone</Form.Label>
-                  <Form.Control name="phone" autoFocus value={addData.phone} onChange={onChange} />
-                </Form.Group>
-              </Col>
-            </Row>
-
-            <Row>
-              <Col xs={18} md={12}>
-                <Form.Group controlId="email">
-                  <Form.Label>Donor Email</Form.Label>
-                  <Form.Control name="email" autoFocus value={addData.email} onChange={onChange} />
-                </Form.Group>
-              </Col>
-            </Row>
-            <Row>
-              <Col xs={18} md={8}>
-                <Form.Group controlId="address1">
-                  <Form.Label>Address 1</Form.Label>
-                  <Form.Control name="address1" autoFocus value={addData.address1} onChange={onChange} />
-                </Form.Group>
-              </Col>
-              <Col xs={18} md={4}>
-                <Form.Group controlId="address2">
-                  <Form.Label>Address 2</Form.Label>
-                  <Form.Control name="address2" autoFocus value={addData.address2} placeholder="Ex: Apt 1" onChange={onChange} />
-                </Form.Group>
-              </Col>
-            </Row>
-
-            <Row>
-              <Col xs={9} md={6}>
-                <Form.Group controlId="city">
-                  <Form.Label>City</Form.Label>
-                  <Form.Control name="city" autoFocus value={addData.city} onChange={onChange} />
-                </Form.Group>
-              </Col>
-              <Col xs={4.5} md={3}>
-                <Form.Group controlId="state">
-                  <Form.Label>State</Form.Label>
-                  <Form.Control name="state" autoFocus value={addData.state} onChange={onChange} />
-                </Form.Group>
-              </Col>
-              <Col xs={4.5} md={3}>
-                <Form.Group controlId="zip">
-                  <Form.Label>Zip Code</Form.Label>
-                  <Form.Control name="zip" autoFocus value={addData.zip} onChange={onChange} />
-                </Form.Group>
-              </Col>
-            </Row>
-          </Form>
-        </Container>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button variant="secondary" onClick={() => setToggleDonorModal(false)}>
-          Close
-        </Button>
-        <Button variant="primary" onClick={onSubmit} type="submit">
-          Submit
-        </Button>
-      </Modal.Footer>
-    </Modal>
-  );
-};
-
-DonorModal.propTypes = {
-  toggleDonorModal: PropTypes.bool.isRequired,
-  setToggleDonorModal: PropTypes.func.isRequired,
-  data: PropTypes.arrayOf(PropTypes.object).isRequired,
-  setData: PropTypes.func.isRequired,
-  row: PropTypes.object,
+  table: PropTypes.object,
+  data: PropTypes.array,
+  setData: PropTypes.func,
 };
 
 const Donors = () => {
   const [data, setData] = useState();
   const [columnFilters, setColumnFilters] = useState([]);
-  const [toggleDonorModal, setToggleDonorModal] = useState(false);
+  const [toggleAddModal, setToggleAddModal] = useState(false);
+  const { search } = useDonor();
+  const params = new URLSearchParams(search);
+  const page = parseInt(params.get('page') ?? '1', 10);
+  const [lastPage, setLastPage] = useState(1);
 
   useEffect(() => {
-    fetch('/api/donors')
-      .then((res) => res.json())
-      .then((data) => setData(data));
-  }, []);
+    Api.donors.index(page).then((response) => {
+      setData(response.data);
+      const linkHeader = Api.parseLinkHeader(response);
+      let newLastPage = page;
+      if (linkHeader?.last) {
+        const match = linkHeader.last.match(/page=(\d+)/);
+        newLastPage = parseInt(match[1], 10);
+      } else if (linkHeader?.next) {
+        newLastPage = page + 1;
+      }
+      setLastPage(newLastPage);
+    });
+  }, [page]);
 
   const table = useReactTable({
     data: data || [],
@@ -351,39 +210,30 @@ const Donors = () => {
     },
     getCoreRowModel: getCoreRowModel(),
     getFilteredRowModel: getFilteredRowModel(),
-    // getSortedRowModel: getSortedRowModel(),
-    getPaginationRowModel: getPaginationRowModel(),
   });
 
   return (
     <main className="container">
       <div className="d-flex justify-content-between align-items-center mt-5">
-        <button type="button" className="btn btn-primary d-flex align-items-center" onClick={() => setToggleDonorModal(true)}>
+        <button type="button" className="btn btn-primary d-flex align-items-center" onClick={() => setToggleAddModal(true)}>
           New <i className="bi bi-plus-lg" />
         </button>
-        <DonorModal
-          toggleDonorModal={toggleDonorModal}
-          setToggleDonorModal={setToggleDonorModal}
-          data={data}
-          setData={setData}
-        />
+        <AddDonorModal toggleAddModal={toggleAddModal} setToggleAddModal={setToggleAddModal} data={data} setData={setData} />
         <i className="bi title-icon bi-box2-heart">Donors</i>
         <Filters setColumnFilters={setColumnFilters} />
       </div>
-      <DonorTable table={table} data={data} setData={setData} setToggleDonorModal={setToggleDonorModal}/>
-      <p>
-        Page {table.getState().pagination.pageIndex + 1} of {table.getPageCount()}
-      </p>
-      <div className="btn-group" role="group">
-        <button type="button" className="btn btn-primary" onClick={() => table.previousPage()} disabled={!table.getCanPreviousPage()}>
-          {'<'}
-        </button>
-        <button type="button" className="btn btn-primary" onClick={() => table.nextPage()} disabled={!table.getCanNextPage()}>
-          {'>'}
-        </button>
-      </div>
+      <DonorTable table={table} data={data} setData={setData} />
+      <Pagination page={page} lastPage={lastPage} />
     </main>
   );
 };
 
 export default Donors;
+
+// import React from 'react';
+
+// const Donors = () => {
+//   return <div>Donors</div>;
+// };
+
+// export default Donors;
