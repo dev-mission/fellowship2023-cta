ef010934ed4deb9948e9db82f2586ec62d707e56
Duan Chen <duanchenwork@gmail.com>
2024-04-09 00:05:53 -0600

    Saving work for laptop

--- server/routes/api/appointments.js
+++ server/routes/api/appointments.js
@@ -10,17 +10,7 @@ const router = express.Router();
 router.get('/', async (req, res) => {
   const page = req.query.page || '1';
   const { records, pages, total } = await models.Appointment.paginate({
-    // include: [models.Client],
     page,
-    order: [
-      ['ClientId', 'ASC'],
-      ['dateTimeAt', 'ASC'],
-      ['UserId', 'ASC'],
-      ['LocationId', 'ASC'],
-      ['DeviceId', 'ASC'],
-      ['problem', 'ASC'],
-      ['status', 'ASC'],
-    ],
   });
   helpers.setPaginationHeaders(req, res, page, pages, total);
   res.json(records.map((r) => r.toJSON()));
@@ -49,7 +39,7 @@ router.get('/:appointment', async (req, res) => {
 router.patch('/:id', async (req, res) => {
   try {
     const record = await models.Appointment.findByPk(req.params.id);
-    await record.update(_.pick(req.body, ['ClientId', 'UserId', 'LocationId', 'DeviceId', 'state', 'zipCode']));
+    await record.update(_.pick(req.body, ['ClientId', 'UserId', 'LocationId', 'state', 'zipCode']));
     res.json(record);
   } catch (err) {
     console.log(err);
