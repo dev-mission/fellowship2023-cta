5291a2da700201f248ca4ef5dd445e074090ec5d
duanhash <122394349+duanhash@users.noreply.github.com>
2024-04-11 18:28:21 -0700

    Combining columns & nested data example (#119)
    * Include example for API
    
    * Ran pp & lint
    
    * Finished defining columns for Appointments Page
    
    * Combining columns & nested data example
    
    ---------
    
    Co-authored-by: duanhash <duanchenwork@gmail.com>

--- client/src/Appointments/Appointments.jsx
+++ client/src/Appointments/Appointments.jsx
@@ -1,35 +1,51 @@
 import { useState, useEffect } from 'react';
-import // getCoreRowModel,
-// getFilteredRowModel,
-// getSortedRowModel,
-// useReactTable,
-// flexRender,
-'@tanstack/react-table';
-// import PropTypes from 'prop-types';
-// import { DeleteModal } from '../Components';
+import { getCoreRowModel, useReactTable } from '@tanstack/react-table';
 import Api from '../Api';
-// import Pagination from '../Components/Pagination';
+import Pagination from '../Components/Pagination';
 import { useLocation } from 'react-router-dom';
 import { ButtonGroup, ToggleButton } from 'react-bootstrap';
+import AppointmentsTable from './AppointmentsTable';
+import AddAppointmentModal from './AddAppointmentModal';
 
-// const columns = [
-//   {
-//     accessorKey: 'ClientId',
-//     header: 'Client',
-//     enableColumnFilter: true,
-//   },
-//   {
-//     accessorKey: 'dateTimeAt',
-//     header: 'Date Time',
-//     enableColumnFilter: true,
-//   },
-//   {
-//     accessorKey: 'UserId',
-//     header: 'User',
-//     enableColumnFilter: true,
-//   },
-//   {},
-// ];
+const columns = [
+  {
+    accessorKey: 'Client.fullName',
+    header: 'Client',
+  },
+  {
+    accessorKey: 'dateTimeAt',
+    header: 'Date Time',
+  },
+  {
+    header: 'User',
+    accessorKey: 'User',
+    cell: ({ row }) => <p>{row.original.User.firstName + ' ' + row.original.User.lastName}</p>,
+  },
+  {
+    accessorKey: 'Client.phone',
+    header: 'Phone',
+  },
+  {
+    accessorKey: 'Client.email',
+    header: 'Email',
+  },
+  {
+    accessorKey: 'Location.name',
+    header: 'Location',
+  },
+  {
+    accessorKey: 'Client.Device.model',
+    header: 'Device',
+  },
+  {
+    accessorKey: 'problem',
+    header: 'Problem',
+  },
+  {
+    accessorKey: 'status',
+    header: 'Status',
+  },
+];
 
 const Appointments = () => {
   const [data, setData] = useState();
@@ -39,9 +55,6 @@ const Appointments = () => {
   const page = parseInt(params.get('page') ?? '1', 10);
   const [lastPage, setLastPage] = useState(1);
   const [radioValue, setRadioValue] = useState('Upcoming');
-
-  console.log(toggleAddModal);
-  console.log(lastPage);
 
   useEffect(() => {
     Api.appointments.index(page).then((response) => {
@@ -60,14 +73,14 @@ const Appointments = () => {
 
   console.log(data);
 
-  // const table = useReactTable({
-  //   data: data || [],
-  //   columns,
-  //   state: {
-  //     data,
-  //   },
-  //   getCoreRowModel: getCoreRowModel(),
-  // });
+  const table = useReactTable({
+    data: data || [],
+    columns,
+    state: {
+      data,
+    },
+    getCoreRowModel: getCoreRowModel(),
+  });
 
   return (
     <main className="container">
@@ -75,7 +88,8 @@ const Appointments = () => {
         <button type="button" className="btn btn-primary d-flex align-items-center" onClick={() => setToggleAddModal(true)}>
           New <i className="bi bi-plus-lg" />
         </button>
-        <i className="bi bi-clock">Appointments</i>
+        <AddAppointmentModal toggleAddModal={toggleAddModal} setToggleAddModal={setToggleAddModal} data={data} setData={setData} />
+        <i className="bi bi-clock title-icon">Appointments</i>
         <form className="d-flex" role="search">
           <div className="input-group">
             <span className="input-group-text" id="basic-addon1">
@@ -85,34 +99,34 @@ const Appointments = () => {
           </div>
         </form>
       </div>
-      <div>
-        <ButtonGroup>
-          <ToggleButton
-            className={`border-primary ${radioValue === 'Upcoming' ? 'text-white' : 'text-primary'} `}
-            id="radio-upcoming"
-            type="radio"
-            variant={radioValue === 'Upcoming' ? 'outline-primary' : ''}
-            name="radio"
-            value="Upcoming"
-            checked={radioValue === 'Upcoming'}
-            onChange={(e) => setRadioValue(e.currentTarget.value)}
-            size="md">
-            Upcoming
-          </ToggleButton>
-          <ToggleButton
-            className={`border-primary ${radioValue === 'Archive' ? 'text-white' : 'text-primary'}`}
-            id="radio-archive"
-            type="radio"
-            variant={radioValue === 'Archive' ? 'outline-primary' : ''}
-            name="radio"
-            value="Archive"
-            checked={radioValue === 'Archive'}
-            onChange={(e) => setRadioValue(e.currentTarget.value)}
-            size="md">
-            Archive
-          </ToggleButton>
-        </ButtonGroup>
-      </div>
+      <ButtonGroup>
+        <ToggleButton
+          className={`border-primary ${radioValue === 'Upcoming' ? 'text-white' : 'text-primary'} `}
+          id="radio-upcoming"
+          type="radio"
+          variant={radioValue === 'Upcoming' ? 'outline-primary' : ''}
+          name="radio"
+          value="Upcoming"
+          checked={radioValue === 'Upcoming'}
+          onChange={(e) => setRadioValue(e.currentTarget.value)}
+          size="md">
+          Upcoming
+        </ToggleButton>
+        <ToggleButton
+          className={`border-primary ${radioValue === 'Archive' ? 'text-white' : 'text-primary'}`}
+          id="radio-archive"
+          type="radio"
+          variant={radioValue === 'Archive' ? 'outline-primary' : ''}
+          name="radio"
+          value="Archive"
+          checked={radioValue === 'Archive'}
+          onChange={(e) => setRadioValue(e.currentTarget.value)}
+          size="md">
+          Archive
+        </ToggleButton>
+      </ButtonGroup>
+      <AppointmentsTable table={table} data={data} setData={setData} />
+      <Pagination page={page} lastPage={lastPage} />
     </main>
   );
 };
--- client/src/Locations/Locations.jsx
+++ client/src/Locations/Locations.jsx
@@ -1,12 +1,11 @@
 import { useState, useEffect } from 'react';
-import { getCoreRowModel, getFilteredRowModel, useReactTable, flexRender } from '@tanstack/react-table';
+import { getCoreRowModel, getFilteredRowModel, useReactTable } from '@tanstack/react-table';
 import PropTypes from 'prop-types';
-import { DeleteModal } from '../Components';
 import AddLocationModal from './AddLocationModal';
-import EditLocationModal from './EditLocationModal';
 import Api from '../Api';
 import Pagination from '../Components/Pagination';
 import { useLocation } from 'react-router-dom';
+import LocationsTable from './LocationsTable';
 
 const columns = [
   {
@@ -65,103 +64,6 @@ Filters.propTypes = {
   setColumnFilters: PropTypes.func,
 };
 
-const LocationTable = ({ table, data, setData }) => {
-  const [toggleDeleteModal, setToggleDeleteModal] = useState(false);
-  const [toggleEditModal, setToggleEditModal] = useState(false);
-  const [editData, setEditData] = useState({});
-  const [propRow, setPropRow] = useState({});
-
-  const onDelete = (row) => (e) => {
-    try {
-      setToggleDeleteModal(true);
-      setPropRow(row);
-    } catch (err) {
-      console.log(err);
-      console.log(e);
-    }
-  };
-
-  const onEdit = (row) => (e) => {
-    try {
-      setToggleEditModal(true);
-      setPropRow(row);
-      setEditData({
-        id: row.original.id,
-        name: row.original.name,
-        address1: row.original.address1,
-        address2: row.original.address2,
-        city: row.original.city,
-        state: row.original.state,
-        zipCode: row.original.zipCode,
-      });
-    } catch (err) {
-      console.log(err);
-      console.log(e);
-    }
-  };
-
-  return (
-    <>
-      <table className="table mt-5">
-        <thead className="table-primary">
-          <tr>
-            {table.getHeaderGroups()[0].headers.map((header) => (
-              <th scope="col" key={header.id}>
-                {header.column.columnDef.header}
-                {/* {header.column.getCanSort() && <i className="ms-2 bi bi-arrow-down-up" onClick={header.column.getToggleSortingHandler()} />} */}
-              </th>
-            ))}
-            <th scope="col">
-              <i className="bi bi-pencil-square" />
-            </th>
-            <th scope="col">
-              <i className="bi bi-trash-fill" />
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {table.getRowModel().rows.map((row) => (
-            <tr key={row.id}>
-              {row.getVisibleCells().map((cell) => (
-                <td key={cell.id}>{flexRender(cell.column.columnDef.cell, cell.getContext())}</td>
-              ))}
-              <td>
-                <i className="bi bi-pencil" onClick={onEdit(row)} />
-              </td>
-              <td>
-                <i className="bi bi-x-lg" onClick={onDelete(row)} />
-              </td>
-            </tr>
-          ))}
-        </tbody>
-        <DeleteModal
-          toggleDeleteModal={toggleDeleteModal}
-          setToggleDeleteModal={setToggleDeleteModal}
-          row={propRow}
-          data={data}
-          setData={setData}
-          model="locations"
-        />
-        <EditLocationModal
-          toggleEditModal={toggleEditModal}
-          setToggleEditModal={setToggleEditModal}
-          row={propRow}
-          data={data}
-          setData={setData}
-          editData={editData}
-          setEditData={setEditData}
-        />
-      </table>
-    </>
-  );
-};
-
-LocationTable.propTypes = {
-  table: PropTypes.object,
-  data: PropTypes.array,
-  setData: PropTypes.func,
-};
-
 const Locations = () => {
   const [data, setData] = useState();
   const [columnFilters, setColumnFilters] = useState([]);
@@ -204,10 +106,10 @@ const Locations = () => {
           New <i className="bi bi-plus-lg" />
         </button>
         <AddLocationModal toggleAddModal={toggleAddModal} setToggleAddModal={setToggleAddModal} data={data} setData={setData} />
-        <i className="bi bi-person-fill">Locations</i>
+        <i className="bi bi-person-fill title-icon">Locations</i>
         <Filters setColumnFilters={setColumnFilters} />
       </div>
-      <LocationTable table={table} data={data} setData={setData} />
+      <LocationsTable table={table} data={data} setData={setData} />
       <Pagination page={page} lastPage={lastPage} />
     </main>
   );
