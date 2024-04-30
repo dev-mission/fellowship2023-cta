387bc23f24170cea887f500d56cc13e959c0e030
Nichole Guan <nicholeguan2@gmail.com>
2024-04-29 18:56:34 -0700

    Fix api endpoint

--- client/src/Clients/Clients.jsx
+++ client/src/Clients/Clients.jsx
@@ -66,7 +66,7 @@ const Clients = () => {
   const [lastPage, setLastPage] = useState(1);
 
   useEffect(() => {
-    Api.courses.index(page).then((response) => {
+    Api.clients.index(page).then((response) => {
       setData(response.data);
       const linkHeader = Api.parseLinkHeader(response);
       let newLastPage = page;
